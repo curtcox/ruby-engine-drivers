01acc8621780546eda54599943497e939a34ee5c
Stephen von Takach <steve@cotag.me>
2016-09-28 23:47:20 +1000

    (engine) ensure orchestrator is loaded first

--- lib/aca-device-modules.rb
+++ lib/aca-device-modules.rb
@@ -1,2 +1,2 @@
-
+require 'orchestrator'
 require 'aca-device-modules/engine'
--- lib/aca-device-modules/engine.rb
+++ lib/aca-device-modules/engine.rb
@@ -1,6 +1,5 @@
 module AcaDeviceModules
     class Engine < ::Rails::Engine
-
         config.after_initialize do |app|
             app.config.orchestrator.module_paths << File.expand_path('../../../modules', __FILE__)
         end
