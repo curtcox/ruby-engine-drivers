934531cc88e04a9a71603b0ceacf8520045b70fc
Kim Burgess <kim@acaprojects.com>
2017-04-12 12:17:28 +1000

    (aca:myturn) fix issue with available source and input lookup

--- modules/aca/myturn.rb
+++ modules/aca/myturn.rb
@@ -8,7 +8,7 @@ class Aca::MyTurn
     implements :logic
 
     def on_load
-        # TODO subscribe to mode changes
+        # TODO: subscribe to mode changes
 
         on_update
     end
@@ -22,36 +22,37 @@ class Aca::MyTurn
     def present(source)
         source = source.to_sym
 
-        # TODO add switching / window layout logic
+        # TODO: add switching / window layout logic
     end
 
     protected
 
-    def source_available?(key)
-        sources = system[:inputs].map { |input| system[input] }.flatten
-        sources.include?(key)
+    def source_available?(sys, key)
+        sources = sys[:inputs].map { |input| sys[input] }.flatten
+        sources.include? key
     end
 
-    def triggers
-        sources = system[:sources].select do |name, config|
-            source_available?(name) && config.key?(:myturn_trigger)
-        end
-
-        sources.transform_values do |details|
-            trigger = details[:myturn_trigger]
-            # Allow module to be specified as either `DigitalIO_1`, or as
-            # discreet module name and index keys.
-            /(?<mod>[^_]+)(_(?<idx>\d+))?/ =~ trigger[:module]
-            {
-                module: mod,
-                index: idx || trigger[:index] || 1,
-                status: trigger[:status],
-                value: trigger[:value]
-            }
-        end
+    def extract_trigger(source)
+        trigger = source[:myturn_trigger]
+        # Allow module to be specified as either `DigitalIO_1`, or as
+        # discreet module name and index keys.
+        /(?<mod>[^_]+)(_(?<idx>\d+))?/ =~ trigger[:module]
+        {
+            module: mod,
+            index: idx || trigger[:index] || 1,
+            status: trigger[:status],
+            value: trigger[:value]
+        }
     end
 
-    def subscribe(source, trigger)
+    def lookup_triggers(sys)
+        sources = sys[:sources].select do |name, config|
+            source_available?(sys, name) && config.key?(:myturn_trigger)
+        end
+        sources.transform_values { |config| extract_trigger(config) }
+    end
+
+    def bind(source, trigger)
         status = trigger.values_at(:module, :index, :status)
         system.subscribe(*status) do |notice|
             if notice.value == trigger[:value]
@@ -62,9 +63,10 @@ class Aca::MyTurn
     end
 
     def rebind
+        sys = system[:System]
         @subscriptions.each { |ref| unsubscribe(ref) } if @subscriptions
-        @subscriptions = triggers.map do |source, trigger|
-            subscribe(source, trigger)
+        @subscriptions = lookup_triggers(sys).map do |source, trigger|
+            bind source, trigger
         end
     end
 end
