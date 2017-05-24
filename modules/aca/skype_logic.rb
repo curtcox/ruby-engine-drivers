5f7a6891931cdb8ee9b9b4d00367f8d7f060189d
Stephen von Takach <steve@cotag.me>
2017-05-24 18:03:18 +1000

    (aca:skype) client to set incoming call state

--- modules/aca/skype_logic.rb
+++ modules/aca/skype_logic.rb
@@ -18,18 +18,16 @@ class Aca::SkypeLogic
         self[:dial_link] = !self[:dial_link]
     end
 
-    def incomming_call(state)
-        self[:incomming_call] = !!state
+    def incoming_call(state)
+        self[:incoming_call] = !!state
     end
 
     def accept_call
         self[:accept_call] = !self[:accept_call]
-        self[:incomming_call] = false
     end
 
     def hang_up
         self[:hang_up] = !self[:hang_up]
-        self[:incomming_call] = false
     end
 
     def show_self(state)
