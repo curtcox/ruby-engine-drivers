903214870fe1186419541db4abf2986e77783db8
Stephen von Takach <steve@cotag.me>
2017-06-14 17:54:00 +1000

    (clear_one:converge) test mute, gain and macro commands

--- modules/clear_one/converge.rb
+++ modules/clear_one/converge.rb
@@ -18,34 +18,93 @@ class ClearOne::Converge
 
 
     default_settings({
-        device_type: '*',
+        # Specifying '*' for device type works however it returns
+        # failures with the success for other devices
+        device_type: 'H',
         device_id: '*',
         username: 'clearone',
         password: 'converge'
     })
 
 
+    DeviceTypes = {
+        converge_880t: 'D',
+        converge_880: '1',
+        converge_th20: '2',
+        converge_840t: '3',
+        converge_8i: 'A',
+        converge_1212: 'G',
+        converge_1212a: 'I',
+        converge_880ta: 'H',
+        converge_vh20: 'E'
+    }
+    DeviceTypes.merge!(DeviceTypes.invert)
+
+    Groups = {
+        input: 'I',
+        output: 'O',
+        mic: 'M',
+        beamforming_mic: 'V',
+        amp: 'J',
+        gating: 'G',
+        processing: 'P',
+        expansion: 'E',
+        line_in: 'L',
+        expansion_ref: 'A',
+        gpio: 'Y',
+        matrix: 'X',
+        fader: 'F',
+        presets: 'S',
+        macros: 'C',
+        transmit: 'T',
+        receive: 'R',
+        virtual_ref: 'B',
+        time_event: 'Q',
+        web: 'W',
+        pa_virtual_ref: 'H',
+        voip_transmit: 'K',
+        voip_receive: 'Z',
+        usb_transmit: 'D',
+        usb_receive: 'U'
+    }
+    Groups.merge!(Groups.invert)
+
+    MeterTypes = {
+        input_level: 'I',
+        post_gain_level: 'A',
+        post_filter_level: 'N',
+        post_gate_level: 'G'
+    }
+    MeterTypes.merge!(MeterTypes.invert)
+
+
     def on_load
         on_update
     end
-    
+
     def on_update
         @device_type = setting(:device_type)
         @device_id = setting(:device_id)
+
+        self[:model] = DeviceTypes[@device_type] || 'any'
     end
-    
-    
+
+
     def connected
         self[:authenticated] = false
+
+        schedule.every('50s') do
+            query_meter 1
+        end
     end
-    
+
     def disconnected
         schedule.clear
     end
-    
-    
+
+
     def preset(number, type = :macro)
-        do_send "#{type} #{number}", name: type.to_sym
+        do_send type, number, name: type.to_sym
     end
     alias_method :macro, :preset
 
@@ -57,77 +116,111 @@ class ClearOne::Converge
         do_send "reboot"
     end
 
-    def get_aliases
-        do_send "SESSION get aliases"
-    end
-
 
     # ---------------------
     # Compatibility Methods
     # ---------------------
-    def fader(fader_id, level, fader_type = 'F', meter_type = 'A')
-        value = in_range(level, 99.99, -99.99)
+    def fader(fader_id, level, fader_type = :fader)
+        value = in_range(level, 20.0, -65.0)
 
         faders = Array(fader_id)
         faders.each do |fad|
-            do_send 'LVL', fad, fader_type, meter_type, value
+            do_send 'GAIN', fad, Groups[fader_type.to_sym], value, 'A'
         end
     end
 
-    def faders(ids:, level:, index: 'F', type: 'A', **_)
+    def faders(ids:, level:, index: :fader, **_)
         fader(ids, level, index, type)
     end
 
-    def query_fader(fader_id, fader_type = 'F', meter_type = 'A')
+    def query_fader(fader_id, fader_type = :fader)
         fad = fader_id.is_a?(Array) ? fader_id[0] : fader_id
-        do_send 'LVL', fad, fader_type, meter_type
+        do_send 'GAIN', fad, Groups[fader_type.to_sym]
     end
 
-    def query_faders(ids:, index: 'F', type: 'A', **_)
-        query_fader(ids, index, type)
+    def query_faders(ids:, index: :fader, **_)
+        query_fader(ids, index)
     end
 
 
-    def mute(mute_id, value = true, fader_type = 'F')
+    def mute(mute_id, value = true, fader_type = :fader)
         level = is_affirmative?(value) ? 1 : 0
+        fad_type = Groups[fader_type.to_sym]
 
         mutes = Array(mute_id)
         mutes.each do |mute|
-            do_send 'MUTE', mute, fader_type, level
+            do_send 'MUTE', mute, fad_type, level
         end
     end
 
-    def unmute(mute_id, fader_type = 'F')
+    def unmute(mute_id, fader_type = :fader)
         mute(mute_id, false, fader_type)
     end
 
-    def mutes(ids:, muted: true, index: 'F', **_)
+    def mutes(ids:, muted: true, index: :fader, **_)
         mute(ids, muted, index)
     end
 
-    def query_mute(mute_id, fader_type = 'F')
+    def query_mute(mute_id, fader_type = :fader)
         fad = mute_id.is_a?(Array) ? mute_id[0] : mute_id
-        do_send 'MUTE', fad, fader_type
+        do_send 'MUTE', fad, Groups[fader_type.to_sym]
     end
 
-    def query_mutes(ids:, index: 'F', **_)
+    def query_mutes(ids:, index: :fader, **_)
         query_mute(ids, index)
     end
 
+
+    def enable_level_reporting(value)
+        level = is_affirmative?(value) ? 1 : 0
+        do_send 'LVLREPORTEN', level
+    end
+
+    def query_meter(fader_id, fader_type = :input, meter_type = :input_level)
+        fad = fader_id.is_a?(Array) ? fader_id[0] : fader_id
+        do_send 'LVL', fad, Groups[fader_type.to_sym], MeterTypes[meter_type.to_sym]
+    end
 
 
     def received(data, resolve, command)
         logger.debug { "received #{data.inspect}" }
 
-        if data == "\n" && !self[:authenticated]
-            schedule.in(300) { send "#{setting(:username)}\r\n", priority: 999 }
-        elsif data =~ /user:/i
-            schedule.in(300) { send "#{setting(:password)}\r\n", priority: 999 }
-        elsif data == 'Authenticated.'
-            self[:authenticated] = true
-        elsif data == 'Invalid User/Pass.'
-            self[:authenticated] = false
-            logger.warn data
+        if !self[:authenticated]
+            if data == "\n"
+                schedule.in(300) { send "#{setting(:username)}\r\n", priority: 999 }
+            elsif data =~ /user/i
+                schedule.in(300) { send "#{setting(:password)}\r\n", priority: 999 }
+            elsif data =~ /Authenticated/i
+                self[:authenticated] = true
+            elsif data =~ /Invalid/i
+                self[:authenticated] = false
+                logger.warn data
+            end
+
+            return :success
+        end
+
+        result = data.split('> ')[-1].split(' ')
+
+        case result[1].downcase.to_sym
+        when :mute
+            id = result[2]
+            type = Groups[result[3]]
+            self["fader#{id}_#{type}_mute"] = result[4] == '1'
+        when :lvl
+            id = result[2]
+            type = Groups[result[3]]
+            meter = MeterTypes[result[4]] || 'unknown_meter'
+            self["#{type}#{id}_#{meter}"] = result[5].to_f
+        when :gain
+            id = result[2]
+            type = Groups[result[3]]
+            self["fader#{id}_#{type}"] = result[4].to_f
+        when :macro
+            self[:last_macro] = result[2].to_i
+        else
+            # Example response: OK> #H0 ERROR ARGUMENT ERROR.
+            logger.error(result.join(' ')) if data =~ /error/i
         end
 
         return :success
@@ -138,8 +231,8 @@ class ClearOne::Converge
 
 
     def do_send(command, *args, **options)
-        logger.debug { "requesting #{command}" }
-        send "##{@device_type}#{@device_id} #{command} #{args.join(' ')}\r", options
+        cmd = "##{@device_type}#{@device_id} #{command} #{args.join(' ')}\r\n"
+        logger.debug { "requesting #{cmd.inspect}" }
+        send cmd, options
     end
 end
-
--- modules/clear_one/converge_spec.rb
+++ modules/clear_one/converge_spec.rb
@@ -7,9 +7,27 @@ Orchestrator::Testing.mock_device 'ClearOne::Converge' do
     transmit "\r\npass"
     wait(350)
     should_send "converge\r\n"
-    
+
     transmit "\r\n\r\nAuthenticated.\r\nText Mode Engaged\r\n"
     transmit "\r\nLevel: Administrator\r\n"
 
     expect(status[:authenticated]).to be(true)
+
+    exec(:preset, 1)
+        .should_send("#H* macro 1\r\n")
+        .responds("OK> #H0 MACRO 1\r\n")
+
+    expect(status[:last_macro]).to be(1)
+
+    exec(:fader, 1, -90, 'mic')
+        .should_send("#H* GAIN 1 M -65.0 A\r\n")
+        .responds("OK> #H0 GAIN 1 M -65.00 A\r\n")
+
+    expect(status[:fader1_mic]).to be(-65.0)
+
+    exec(:mute, 1)
+        .should_send("#H* MUTE 1 F 1\r\n")
+        .responds("OK> #H0 MUTE 1 F 1\r\n")
+
+    expect(status[:fader1_fader_mute]).to be(true)
 end
