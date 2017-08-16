b3ee70410fe568a0caad44df372dbbcc5dc4f6e1
Stephen von Takach <steve@cotag.me>
2017-08-16 20:39:04 +1000

    (cisco:sx series codec) fix telnet protocol on EngineV1
    communication settings are not inherited on EngineV1 - not an issue on
    EngineV2 however it uses the SSH modules anyway.

--- modules/cisco/tele_presence/sx_series.rb
+++ modules/cisco/tele_presence/sx_series.rb
@@ -5,6 +5,11 @@ load File.expand_path('./sx_series_common.rb', File.dirname(__FILE__))
 class Cisco::TelePresence::SxSeries < Cisco::TelePresence::SxTelnet
     include Cisco::TelePresence::SxSeriesCommon
 
+    # Communication settings
+    tokenize delimiter: "\r",
+             wait_ready: "login:"
+    clear_queue_on_disconnect!
+
     # Discovery Information
     descriptive_name 'Cisco TelePresence'
     generic_name :VidConf
