50e42dcfc6ad817cca866707b70c01620baddc4f
Cameron Reeves <c@meronreev.es>
2017-11-27 13:51:45 +1100

    Update domino library to do CRUD

--- lib/ibm/domino.rb
+++ lib/ibm/domino.rb
@@ -1,14 +1,3 @@
-# domino = IBM::Domino.new({
-#     username: nil,
-#     password: nil,
-#     auth_hash: 'T1RTIFRlc3QxIFByb2plY3QgU0c6UEBzc3cwcmQxMjM=',
-#     domain: 'http://sg-mbxpwv001.cn.asia.ad.pwcinternal.com',
-#     timezone: 'Singapore'
-# })
-# res = nil
-# reactor.run {
-#     res = domino.get_bookings('mail/generic/otstest1projectsg.nsf', (Time.now - 1.day).midnight, (Time.now - 1.day).midnight + 3.days)
-# }
 require 'active_support/time'
 module IBM; end
 
@@ -21,6 +10,7 @@ class IBM::Domino
             timezone:
         )
         @domain = domain
+        @timeone = timezone
         @headers = {
             'Authorization' => "Basic #{auth_hash}",
             'Content-Type' => 'application/json'
@@ -28,17 +18,34 @@ class IBM::Domino
         @domino_api = UV::HttpEndpoint.new(@domain, {inactivity_timeout: 25000})
     end
 
-    def to_ibm_date(time)
-        time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    def domino_request(request_method, endpoint, data = nil, query = {}, headers = {})
+        # Convert our request method to a symbol and our data to a JSON string
+        request_method = request_method.to_sym
+        data = data.to_json if !data.nil? && data.class != String
+
+        @headers.merge(headers) if headers  
+
+        domino_path = "#{ENV['DOMINO_DOMAIN']}#{endpoint}"
+        response = @domino_api.__send__(request_method, path: domino_path, headers: @headers, body: data, query: query)
+    end
+
+    def get_free_rooms(starting, ending)
+        starting, ending = get_time_range(starting, ending, @timezone)
+
+        req_params = {
+            :site => ENV["DOMINO_SITE"],
+            :start => to_ibm_date(starting),
+            :end => to_ibm_date(ending)
+        }
+
+        res = domino_request('get','/api/freebusy/freerooms', nil, req_params).value
+        domino_emails = JSON.parse(res.body)['rooms']
     end
 
 
+
     def get_bookings(database, starting, ending, days=nil)
-
-        # Set our unchanging path
-        path = "#{@domain}/#{database}/api/calendar/events"
-
-        # Sent count to max
+        # Set count to max
         query = {
             count: 100
         }
@@ -49,29 +56,118 @@ class IBM::Domino
             query[:before] = to_ibm_date(ending)
         end
 
-        response = @domino_api.get(path: path, headers: @headers, body: nil, query: query).value
+        # Get our bookings 
+        response = domino_request('get', "/#{database}/api/calendar/events", nil, query).value
         domino_bookings = JSON.parse(response.body)['events']
 
+        # Grab the attendee for each booking
         bookings = []
-
         domino_bookings.each{ |booking|
-            # booking = domino_bookings.sample
             bookings.push(get_attendees(booking, database))
         }
         bookings
     end
 
+
+    def create_booking(starting:, ending:, room:, summary:, description: nil, organizer:, attendees: [], timezone: @timezone, **opts)
+        event = {
+            :summary => summary,
+            :class => :public,
+            :start => to_utc_date(starting),
+            :end => to_utc_date(ending)
+        }
+
+        event[:description] = description if description
+
+
+        event[:attendees] = Array(attendees).collect do |attendee|
+            {
+                role: "req-participant",
+                status: "needs-action",
+                rsvp: true,
+                displayName: attendee[:name],
+                email: attendee[:email]
+            }
+        end
+
+        event[:organizer] = {
+            email: organizer[:email],
+            displayName: organizer[:name]
+        }
+
+        # If there are attendees add the service account
+        event[:attendees].push({
+             "role":"chair",
+             "status":"accepted",
+             "rsvp":false,
+             "displayName":"OTS Test1 Project SG/SG/R&R/PwC",
+             "email":"ots.test1.project.sg@sg.pwc.com"
+        }) if attendees    
+
+        request = domino_request('post', "/#{room}/api/calendar/events", {events: [event]}).value
+        JSON.parse(request.body)['events'][0]
+    end
+
+    def delete_booking(room, id)
+        request = domino_request('delete', "/#{room}/api/calendar/events/#{id}").value.status
+    end
+
+
+    def edit_booking(id, starting:, ending:, room:, summary:, description: nil, organizer:, attendees: [], timezone: @timezone, **opts)
+        event = {
+            :summary => summary,
+            :class => :public,
+            :start => to_utc_date(starting),
+            :end => to_utc_date(ending)
+        }
+
+        query = {}
+
+        if description
+            event[:description] = description
+        else
+            query[:literally] = true
+        end
+
+
+        event[:attendees] = Array(attendees).collect do |attendee|
+            {
+                role: "req-participant",
+                status: "needs-action",
+                rsvp: true,
+                displayName: attendee[:name],
+                email: attendee[:email]
+            }
+        end
+
+        event[:organizer] = {
+            email: organizer[:email],
+            displayName: organizer[:name]
+        }
+
+        # If there are attendees add the service account
+        event[:attendees].push({
+             "role":"chair",
+             "status":"accepted",
+             "rsvp":false,
+             "displayName":"OTS Test1 Project SG/SG/R&R/PwC",
+             "email":"ots.test1.project.sg@sg.pwc.com"
+        }) if attendees    
+
+        request = domino_request('put', "/#{room}/api/calendar/events/#{id}", {events: [event]}, query).value
+        request.status
+    end
+
     def get_attendees(booking, database)
         path = "#{@domain}/#{database}/api/calendar/events/#{booking['id']}"
-        # puts "Attendee path is #{path}"
         booking_request = @domino_api.get(path: path, headers: @headers).value
         booking_response = JSON.parse(booking_request.body)['events'][0]
         if booking_response['attendees']
             attendees = booking_response['attendees'].dup 
-            attendees.map!{ |b|
+            attendees.map!{ |attendee|
                 {
-                    name: b['displayName'],
-                    email: b['email']
+                    name: attendee['displayName'],
+                    email: attendee['email']
                 }
             }
             booking_response['attendees'] = attendees
@@ -87,5 +183,28 @@ class IBM::Domino
         end
         booking_response
     end
-end
 
+    def to_ibm_date(time)
+        time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    end
+
+
+    def to_utc_date(time)
+        utctime = time.getutc
+        {
+            date: utctime.strftime("%Y-%m-%d"),
+            time: utctime.strftime("%H:%M:%S"),
+            utc: true
+        }
+    end
+
+    def get_time_range(starting, ending, timezone)
+        return [starting, ending] if starting.is_a?(Time)
+
+        Time.zone = timezone
+        start = starting.nil? ? Time.zone.today.to_time : Time.zone.parse(starting)
+        fin = ending.nil? ? Time.zone.tomorrow.to_time : Time.zone.parse(ending)
+        [start, fin]
+    end
+
+end
