07b265c9d6d29121bedbc09edbf03216a01d93b7
Kim Burgess <kim@simple.industries>
2017-12-12 19:31:16 +1000

    (cisco:spark) add device implementation for SX80

---
+++ modules/cisco/spark/sx80.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+load File.join(__dir__, 'room_os.rb')
+
+class Cisco::Spark::Sx80 < Cisco::Spark::RoomOs
+    include ::Orchestrator::Security
+    include ::Cisco::Spark::Xapi::Mapper
+
+    descriptive_name 'Cisco Spark SX80'
+    description <<~DESC
+        Device access requires an API user to be created on the endpoint.
+    DESC
+
+    tokenize delimiter: Tokens::COMMAND_RESPONSE,
+             wait_ready: Tokens::LOGIN_COMPLETE
+    clear_queue_on_disconnect!
+
+    protect_method :xcommand, :xconfigruation, :xstatus
+
+    command 'Audio Microphones Mute' => :mic_mute_on
+    command 'Audio Microphones Unmute' => :mic_mute_off
+    command 'Audio Microphones ToggleMute' => :mic_mute_toggle
+    state '/Status/Audio/Microphones/Mute' => :mic_mute
+
+    command 'Audio Sound Play' => :play_sound,
+            Sound: [:Alert, :Bump, :Busy, :CallDisconnect, :CallInitiate,
+                    :CallWaiting, :Dial, :KeyInput, :KeyInputDelete, :KeyTone,
+                    :Nav, :NavBack, :Notification, :OK, :PresentationConnect,
+                    :Ringing, :SignIn, :SpecialInfo, :TelephoneCall,
+                    :VideoCall, :VolumeAdjust, :WakeUp],
+            Loop_: [:Off, :On]
+    command 'Audio Sound Stop' => :stop_sound
+
+    command 'Call Disconnect' => :disconnect, CallId_: Integer
+    command 'Dial' => :dial,
+            Number:  String,
+            Protocol_: [:H320, :H323, :Sip, :Spark],
+            CallRate_: (64..6000),
+            CallType_: [:Audio, :Video]
+
+    command 'Camera PositionReset' => :camera_position_reset,
+            CameraId: (1..7),
+            Axis_: [:All, :Focus, :PanTilt, :Zoom]
+    command 'Camera Ramp' => :camera_move,
+            CameraId: (1..7),
+            Pan_: [:Left, :Right, :Stop],
+            PanSpeed_: (1..15),
+            Tilt_: [:Down, :Up, :Stop],
+            TiltSpeed_: (1..15),
+            Zoom_: [:In, :Out, :Stop],
+            ZoomSpeed_: (1..15),
+            Focus_: [:Far, :Near, :Stop]
+
+    command! 'Cameras AutoFocus Diagnostics Start' => \
+             :autofocus_diagnostics_start,
+             CameraId_: (1..7)
+    command! 'Cameras AutoFocus Diagnostics Stop' => \
+             :autofocus_diagnostics_stop,
+             CameraId_: (1..7)
+    state '/Status/Cameras/Camera' => :camera
+
+    command! 'Cameras PresenterTrack ClearPosition' => :presenter_track_clear
+    command! 'Cameras PresenterTrack StorePosition' => :presenter_track_store
+    command! 'Cameras PresenterTrack Set' => :presenter_track,
+             Mode: [:Off, :Follow, :Diagnostic, :Background, :Setup,
+                    :Persistant]
+    state '/Status/Cameras/PresenterTrack' => :presenter_track
+
+    command! 'Cameras SpeakerTrack Diagnostics Start' => \
+             :speaker_track_diagnostics_start
+    command! 'Cameras SpeakerTrack Diagnostics Stop' => \
+             :speaker_track_diagnostics_stop
+    state '/Status/Cameras/SpeakerTrack' => :speaker_track
+
+    state '/Status/Conference/DoNotDisturb' => :do_not_disturb
+
+    state '/Status/Conference/Presentation/Mode' => :presentation
+
+    state '/Status/Peripherals/ConnectedDevice' => :peripherals
+
+    state '/Status/Video/Input' => :video_input
+
+    state '/Status/Video/Output' => :video_output
+
+    command 'Standby Deactivate' => :wake_up
+    command 'Standby Activate' => :standby
+    command 'Standby ResetTimer' => :reset_standby_timer, Delay: (1..480)
+    state '/Status/Standby/State' => :standby
+
+    command! 'SystemUnit Boot' => :reboot, Action_: [:Restart, :Shutdown]
+end
