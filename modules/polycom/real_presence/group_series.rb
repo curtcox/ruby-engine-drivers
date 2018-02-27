9feb4a6d82813fc5faed118972abc9e9009614d8
Stephen von Takach <steve@cotag.me>
2018-02-27 18:36:41 +1100

    (polycom:group series) add configuration commands

--- modules/polycom/real_presence/group_series.rb
+++ modules/polycom/real_presence/group_series.rb
@@ -24,11 +24,17 @@ class Polycom::RealPresence::GroupSeries
     end
 
     def on_update
-
+        # TODO:: select between gaddrbook (global but not skype)
+        # addrbook (local) commands
+        # globaldir (includes LDAP and Skype)
     end
 
     def connected
+        # Login
+        send "#{setting(:password)}\r", priority: 99
+
         register
+        status
         schedule.every('50s') do
             logger.debug 'Maintaining connection..'
             maintain_connection
@@ -39,10 +45,16 @@ class Polycom::RealPresence::GroupSeries
         schedule.clear
     end
 
-    protect_method :reboot, :reset, :whoami, :unregister
+    protect_method :reboot, :reset, :whoami, :unregister, :register
+    protect_method :maintain_connection, :powerdown, :notify, :nonotify
 
     def reboot
         send "reboot now\r", name: :reboot
+    end
+
+    # Requires manual power on once powered down
+    def powerdown
+        send "powerdown\r"
     end
 
     def reset
@@ -61,15 +73,8 @@ class Polycom::RealPresence::GroupSeries
         send "all register\r", name: :register
     end
 
-    def maintain_connection
-        # Queries the AMX beacon state.
-        send "amxdd get\r", name: :connection_maintenance, priority: 0
-    end
-
-    def answer
-        send "answer video\r", name: :answer
-    end
-
+    # Lists the notification types that are currently being
+    # received, or registers to receive status notifications
     def notify(event)
         send "notify #{event}\r"
     end
@@ -78,18 +83,103 @@ class Polycom::RealPresence::GroupSeries
         send "nonotify #{event}\r"
     end
 
+    def maintain_connection
+        # Queries the AMX beacon state.
+        send "amxdd get\r", name: :connection_maintenance, priority: 0
+    end
+
+
+
+    def answer
+        send "answer video\r", name: :answer
+    end
+
+    def hangup(call_id = nil)
+        if call_id
+            send "hangup video #{call_id}\r"
+        else
+            send "hangup all\r", name: :hangup_all
+        end
+    end
+
+    # Monitor1: far|near-or-far|content-or-far|all
+    # Monitor2: near|far|content|near-or-far|content-or-near|content-or-far|all
+    # Monitor3: rec-all|rec-far-or-near|near|far|content
+    def configpresentation(setting, monitor: 1)
+        send "configpresentation monitor#{monitor} #{setting}\r"
+    end
+
+    def contentauto?
+        send "contentauto get\r"
+    end
+
+    def contentauto(enabled = true)
+        setting = is_affirmative?(enabled) ? 'on' : 'off'
+        send "contentauto #{setting}\r"
+    end
+
+    def daylightsavings?
+        send "daylightsavings get\r"
+    end
+
+    def daylightsavings(enabled = true)
+        setting = is_affirmative?(enabled) ? 'yes' : 'no'
+        send "daylightsavings #{setting}\r"
+    end
+
+    def defaultgateway?
+        send "defaultgateway get\r"
+    end
+
+    def dial_phone(number)
+        send "dial phone auto \"number\"\r", name: :dial_phone
+    end
+
+    def dial_addressbook(entry)
+        send "dial addressbook \"#{entry}\"\r", name: :dial_address
+    end
+
     def received(response, resolve, command)
         logger.debug { "Polycom sent #{response}" }
 
         # Ignore the echo
-        if command && command[:wait_count] == 0
-            return :ignore
+        parts = response.split(/\s|:\s*/)
+        return :ignore if parts[0].nil?
+
+        cmd = parts[0].downcase.to_sym
+
+        case cmd
+        when :inacall, :autoanswerp2p, :remotecontrol, :microphones,
+                :visualboard, :globaldirectory, :ipnetwork, :gatekeeper,
+                :sipserver, :logthreshold, :meetingpassword, :rpms
+            self[cmd] = parts[1]
+        when :model
+            self[cmd] = parts[1..-1].join(' ')
+        when :serial
+            self[cmd] = parts[2] if parts[1] == 'Number'
+        when :software
+            self[:version] = parts[2] if parts[1] == 'Version'
+        when :build
+            self[cmd] = parts[2]
+        when :notification
+            type = parts[1].to_sym
+            case type
+            when :callstatus
+                if parts[2] == 'outgoing'
+                    if parts.include?('connecting')
+                        # dialing
+                    elsif parts.include?('connected')
+                        # in call
+                    end
+                else # incoming
+
+                end
+            when :mutestatus
+                if parts[2] == 'near'
+                    self[:muted] = parts[-1] == 'muted'
+                end
+            end
         end
-
-        # Break up the message
-        parts = response.split(/\s/)
-
-
 
         :success
     end
