053bf049b770f98595d6f41429a7c09f36d4ad67
Stephen von Takach <steve@cotag.me>
2018-03-01 09:28:02 +1100

    (polycom:group series) finish implementing commands

--- modules/polycom/real_presence/group_series.rb
+++ modules/polycom/real_presence/group_series.rb
@@ -34,6 +34,11 @@ class Polycom::RealPresence::GroupSeries
         send "#{setting(:password)}\r", priority: 99
 
         register
+        send "callstate register\r"
+        send "mute register\r"
+        send "sleep register\r"
+        send "vcbutton register\r"
+        send "volume register\r"
         status
         schedule.every('50s') do
             logger.debug 'Maintaining connection..'
@@ -88,8 +93,6 @@ class Polycom::RealPresence::GroupSeries
         send "amxdd get\r", name: :connection_maintenance, priority: 0
     end
 
-
-
     def answer
         send "answer video\r", name: :answer
     end
@@ -109,26 +112,165 @@ class Polycom::RealPresence::GroupSeries
         send "configpresentation monitor#{monitor} #{setting}\r"
     end
 
-    def contentauto?
-        send "contentauto get\r"
+    OnOffCMDs = {
+        video_mute: 'videomute near',
+        audio_mute: 'mute near',
+        content_auto: 'contentauto',
+        acoustic_fence: 'enableacousticfence',
+        audio_add: 'enableaudioadd',
+        firewall_traversal: 'enablefirewalltraversal',
+        auto_show_content: 'autoshowcontent',
+        basic_mode: 'basicmode',
+        sip_keepalive: 'enablesipka',
+        visual_security: 'enablevisualsecurity',
+        display_far_name: 'farnametimedisplay',
+        global_directory: 'gdsdirectory',
+        generate_tone: 'generatetone',
+        lync_directory: 'lyncdirectory',
+        near_loop: 'nearloop',
+        sleep_mute: 'sleep mute',
+        visual_board: 'visualboard',
+        visual_board_ppt: 'visualboardppt',
+        visual_board_swipe: 'visualboardswipe',
+        visual_board_zoom: 'visualboardzoom'
+    }
+
+    YesNoCMDs = {
+        audio_add: 'enableaudioadd',
+        auto_answer: 'autoanswer',
+        calendar_play_tone: 'calendarplaytone',
+        calendar_register_with_server: 'calendarregisterwithserver',
+        calendar_show_private_meetings: 'calendarshowpvtmeetings',
+        daylight_savings: 'daylightsavings',
+        dynamic_bandwidth: 'dynamicbandwidth',
+        echo_canceller: 'echocanceller',
+        echo_reply: 'echoreply',
+        keyboard_noise_reduction: 'enablekeyboardnoisereduction',
+        live_music_mode: 'enablelivemusicmode',
+        error_concealment: 'enablepvec',
+        resource_reservation: 'enablersvp',
+        snmp_enabled: 'enablesnmp',
+        encryption: 'encryption',
+        far_control_near: 'farcontrolnearcamera',
+        h239_enabled: 'h239enable',
+        multipoint_auto_answer: 'mpautoanswer'
+        mute_auto_answer: 'muteautoanswer'
+    }
+
+    LookupCMD = {}
+    OnOffCMDs.each do |key, value|
+        LookupCMD[value.split(' ')[0]] = key
+
+        define_method key do |enabled|
+            enabled = is_affirmative?(value) ? 'on' : 'off'
+            send "#{value} #{enabled}\r"
+        end
+
+        define_method "#{key}?" do
+            send "#{value} get\r"
+        end
+    end
+    YesNoCMDs.each do |key, value|
+        LookupCMD[value] = key
+
+        define_method key do |enabled|
+            enabled = is_affirmative?(value) ? 'yes' : 'no'
+            send "#{value} #{enabled}\r"
+        end
+
+        define_method "#{key}?" do
+            send "#{value} get\r"
+        end
     end
 
-    def contentauto(enabled = true)
-        setting = is_affirmative?(enabled) ? 'on' : 'off'
-        send "contentauto #{setting}\r"
+    GetOnlyCMDs = {
+        calendar_status: 'calendarstatus',
+        default_gateway: 'defaultgateway',
+        calendar_user: 'calendaruser'
+    }
+
+    GetOnlyCMDs.each do |key, cmd|
+        define_method "#{key}?" do
+            send "#{value} get\r"
+        end
     end
 
-    def daylightsavings?
-        send "daylightsavings get\r"
+    SystemSettings = {
+        connection_preference: 'connectionpreference',
+        dialing_method: 'dialingmethod',
+        primary_camera: 'primarycamera',
+        camera_content: 'cameracontent',
+        camera_content1: 'cameracontent1',
+        camera_content2: 'cameracontent2',
+        camera_content3: 'cameracontent3',
+        display_icons_in_call: 'displayiconsincall',
+        enable_poly_commics: 'enablepolycommics',
+        line_in_level: 'lineinlevel',
+        line_out_mode: 'lineoutmode',
+        media_in_level: 'mediainlevel',
+        self_view: 'selfview',
+        sip_account_name: 'sipaccountname',
+        sip_enable: 'sipenable',
+        stereo_enable: 'stereoenable',
+        transcoding_enabled: 'transcodingenabled',
+        web_enabled: 'webenabled'
+    }
+
+    LookupSetting = SystemSettings.invert
+
+    SystemSettings.each do |key, cmd|
+        define_method key do |value|
+            send "systemsetting #{cmd} #{value}\r"
+        end
+
+        define_method "#{key}?" do
+            send "systemsetting get #{cmd}\r"
+        end
     end
 
-    def daylightsavings(enabled = true)
-        setting = is_affirmative?(enabled) ? 'yes' : 'no'
-        send "daylightsavings #{setting}\r"
+    def button_press(*keys)
+        # succeeded / failed or completed when some keys are not valid
+        send "button #{keys.join(' ')}\r"
     end
 
-    def defaultgateway?
-        send "defaultgateway get\r"
+    def dtmf(char)
+        send "gendial #{char}\r"
+    end
+
+    def video_unmute
+        video_mute false
+    end
+
+    def volume(value)
+        value = in_range(value.to_i, 50)
+        send "volume set #{value}\r"
+    end
+
+    def volume?
+        send "volume get\r"
+    end
+
+    # VC returns the string of text sent
+    def echo(text)
+        send "echo \"#{text}\"\r"
+    end
+
+    def call_info(call_id = nil)
+        if call_id
+            # callinfo begin
+            # callinfo:43:Polycom Group Series Demo:192.168.1.101:384:connected:notmuted:outgoing:videocall
+            # callinfo end
+            send "callinfo all\r"
+        else
+            # callinfo:36:192.168.1.102:256:connected:muted:outgoing:videocall
+            send "callinfo callid #{call_id}\r"
+        end
+    end
+
+    def call_state
+        # cs: call[34] speed[384] dialstr[192.168.1.101] state[connected]
+        # cs: call[1] inactive
+        send "getcallstate\r"
     end
 
     def dial_phone(number)
@@ -139,12 +281,78 @@ class Polycom::RealPresence::GroupSeries
         send "dial addressbook \"#{entry}\"\r", name: :dial_address
     end
 
-    def received(response, resolve, command)
-        logger.debug { "Polycom sent #{response}" }
+    def dial_addressbook_uid(entry)
+        send "dial addressbook_entry #{entry}\r", name: :dial_address
+    end
 
-        # Ignore the echo
+    def multi_point_mode?
+        multi_point_mode(:get)
+    end
+
+    def multi_point_mode(mode)
+        # auto|discussion|presentation|fullscreen
+        send "mpmode #{mode}\r"
+    end
+
+    def recent_calls
+        send "recentcalls\r"
+    end
+
+    def sleep_time?
+        sleep_time(:get)
+    end
+
+    def sleep_time(time)
+        # 0|1|3|15|30|60|120|240|480
+        send "sleeptime #{time}\r"
+    end
+
+    # :none, :local, :remote
+    def presentation_mode(action, source = nil)
+        action = action.to_sym
+
+        self[:presentation] = action
+
+        # play, stop
+        if action == :remote
+            send "vcbutton #{[action, source].compact.join(' ')}\r"
+        else
+            send "vcbutton stop\r"
+        end
+    end
+
+    def presentation_mode?
+        send "vcbutton get\r"
+    end
+
+    def quality_preference?
+        quality_preference(:get)
+    end
+
+    def quality_preference(value)
+        # content|people|both
+        send "vgaqualitypreference #{value}\r"
+    end
+
+    def wake
+        send "wake\r"
+    end
+
+    def received(response, resolve, command)
+        logger.debug { "Polycom sent: #{response}" }
+
+        # Doesn't matter where the event came from
+        if response.start_with?('Control event: ')
+            response = response.split('Control event: ')[1]
+        end
+
         parts = response.split(/\s|:\s*/)
         return :ignore if parts[0].nil?
+
+        if parts[-1] == 'failed'
+            logger.warn "Command failed: #{response}"
+            return :abort
+        end
 
         cmd = parts[0].downcase.to_sym
 
@@ -178,6 +386,31 @@ class Polycom::RealPresence::GroupSeries
                 if parts[2] == 'near'
                     self[:muted] = parts[-1] == 'muted'
                 end
+            end
+        when :mpmode
+            self[:multi_point_mode] = parts[1]
+        when :sleeptime
+            self[:sleep_time] = parts[1].to_i
+        when :systemsetting
+            key = LookupSetting[parts[1]]
+            if key
+                self[key] = parts[-1]
+            end
+        when :vcbutton
+            if parts[1] == 'play'
+                self[:presentation] = :remote
+            elsif parts[1] == 'stop'
+                self[:presentation] = :local if self[:presentation] == :remote
+            end
+        when :vgaqualitypreference
+            self[:quality_preference] = parts[1]
+        when :volume
+            self[:volume] = parts[1].to_i
+        else
+            # Assign yes/no and on/off values
+            key = LookupCMD[parts[0]]
+            if key
+                self[key] = is_affirmative?(parts[-1])
             end
         end
 
