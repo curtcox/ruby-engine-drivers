2eed9de7e8911f9cc6756860e18d9423dafd454b
Stephen von Takach <steve@cotag.me>
2018-03-26 22:14:06 +1100

    (aca:meeting) select confidence monitor automatically
    Track selected camera source

--- modules/aca/meeting_room.rb
+++ modules/aca/meeting_room.rb
@@ -23,6 +23,7 @@ class Aca::MeetingRoom < Aca::Joiner
             self[:Camera] = setting(:Camera)
             self[:hide_vc_sources] = setting(:hide_vc_sources)
             self[:mics_mutes] = setting(:mics_mutes)
+            @confidence_monitor = setting(:confidence_monitor)
 
             # Get any default settings
             @defaults = setting(:defaults) || {}
@@ -895,7 +896,8 @@ class Aca::MeetingRoom < Aca::Joiner
         end
     end
 
-    def select_camera(input, output = nil)
+    def select_camera(source, input, output = nil)
+        self[:selected_camera] = source
         if output
             system[:Switcher].switch({input => output})
         else
@@ -939,6 +941,10 @@ class Aca::MeetingRoom < Aca::Joiner
 
         # So we can keep the UI in sync
         self[:vc_content_source] = inp
+
+        if @confidence_monitor
+            present_actual(inp, @confidence_monitor)
+        end
     end
 
     def wake_pcs
