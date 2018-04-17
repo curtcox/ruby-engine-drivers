e1ab547446d59cc6f0e343b216463be774f7cb2b
Stephen von Takach <steve@cotag.me>
2018-04-17 11:48:56 +1000

    (luton:lighting) complete API

--- modules/lutron/lighting.rb
+++ modules/lutron/lighting.rb
@@ -3,6 +3,9 @@ module Lutron; end
 # Login #1: nwk
 # Login #2: nwk2
 
+# Login: lutron
+# Password: integration
+
 class Lutron::Lighting
     include ::Orchestrator::Constants
     include ::Orchestrator::Transcoder
@@ -18,22 +21,32 @@ class Lutron::Lighting
     delay between_sends: 100
 
     def on_load
+        on_update
     end
 
     def on_unload
     end
 
     def on_update
+        @login = setting(:login) || 'nwk'
+        @trigger_type = setting(:trigger) || :area
     end
 
     def connected
-        schedule.every('60s') do
+        send "#{@login}\r\n", priority: 9999
+
+        schedule.every('40s') do
             logger.debug "-- Polling Lutron"
+
         end
     end
 
     def disconnected
         schedule.clear
+    end
+
+    def restart
+        send_cmd 'RESET', 0
     end
 
     # on or off
@@ -42,26 +55,149 @@ class Lutron::Lighting
         light_level(device, level, 1, 0)
     end
 
-    # light fading
-    def light_level(device, level, action = 1, rate = 1000)
+
+    # ===============
+    # OUTPUT COMMANDS
+    # ===============
+
+    # dimmers, CCOs, or other devices in a system that have a controllable output
+    def level(device, level, rate = 1000, component = :output)
         level = in_range(level.to_i, 100)
         seconds = (rate.to_i / 1000).to_i
         min = seconds / 60
         seconds = seconds - (min * 60)
         time = "#{min.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
-        send_cmd 'OUTPUT', device, action, level, time
+        send_cmd component.to_s.upcase, device, 1, level, time
     end
 
-    # recall a preset (button action)
-    def trigger(area, action)
-        # Level = 200–232 for Scene Number =0–32
-        send_cmd 'AREA', area, 12, (action.to_i + 200)
+    def blinds(device, action, component = :area)
+        case action.to_s.downcase
+        when 'raise', 'up'
+            send_cmd component.to_s.upcase, device, 2
+        when 'lower', 'down'
+            send_cmd component.to_s.upcase, device, 3
+        when 'stop'
+            send_cmd component.to_s.upcase, device, 4
+        end
     end
+
+
+    # =============
+    # AREA COMMANDS
+    # =============
+    def scene(area, scene, component = :area)
+        send_cmd component.to_s.upcase, area, 6, scene
+    end
+
+    def scene?(area, component = :area)
+        send_query component.to_s.upcase, area, 6
+    end
+
+    def occupancy?(area)
+        send_query 'AREA', area, 8
+    end
+
+    def daylight_mode?(area)
+        send_query 'AREA', area, 7
+    end
+
+    def daylight(area, mode)
+        val = is_affirmative?(mode) ? 1 : 2
+        send_cmd 'AREA', area, 7, val
+    end
+
+
+    # ===============
+    # DEVICE COMMANDS
+    # ===============
+    def button_press(area, button)
+        send_cmd 'DEVICE', area, button, 3
+    end
+
+    def led(area, device, state)
+        val = if state.is_a?(Integer)
+            state
+        else
+            is_affirmative?(state) ? 1 : 0
+        end
+
+        send_cmd 'DEVICE', area, device, 9, val
+    end
+
+    def led?(area, device)
+        send_query 'DEVICE', area, device, 9
+    end
+
+
+    # =============
+    # COMPATIBILITY
+    # =============
+    def trigger(area, scene)
+        scene(area, scene, @trigger_type)
+    end
+
+    def light_level(area, level, component = nil, fade = 1000)
+        if component
+            level(area, level, rate = 1000, component)
+        else
+            level(area, level, rate = 1000, :area)
+        end
+    end
+
+
+    Errors = {
+        '1' => 'Parameter count mismatch',
+        '2' => 'Object does not exist',
+        '3' => 'Invalid action number',
+        '4' => 'Parameter data out of range',
+        '5' => 'Parameter data malformed',
+        '6' => 'Unsupported Command'
+    }
+
+    Occupancy = {
+        '1' => :unknown,
+        '2' => :inactive,
+        '3' => :occupied,
+        '4' => :unoccupied
+    }
 
     def received(data, resolve, command)
         logger.debug { "Lutron sent: #{data}" }
 
-        
+        parts = data.split(',')
+        component = parts[0][1..-1].downcase
+
+        case component.to_sym
+        when :area, :output, :shadegrp
+            area = parts[1]
+            action = parts[2].to_i
+            param = parts[3]
+
+            case action
+            when 1 # level
+                self[:"#{component}#{area}_level"] = param.to_f
+            when 6 # Scene
+                self[:"#{component}#{area}"] = param.to_i
+            when 7
+                self[:"#{component}#{area}_daylight"] = param == '1'
+            when 8
+                self[:"#{component}#{area}_occupied"] = Occupancy[param]
+            end
+        when :device
+            area = parts[1]
+            device = parts[2]
+            action = parts[3].to_i
+
+            case action
+            when 7 # Scene
+                self[:"device#{area}_#{device}"] = parts[4].to_i
+            when 9 # LED state
+                self[:"device#{area}_#{device}_led"] = parts[4].to_i
+            end
+        when :error
+            logger.warn "error #{parts[1]}: #{Errors[parts[1]]}"
+            return :abort
+        end
 
         return :success
     end
