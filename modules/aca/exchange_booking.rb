d020797e09b7a23d8cac5a5121df272c1b67ad5a
Cameron Reeves <c@meronreev.es>
2018-04-30 14:57:27 +1000

    Add description setting to exchange driver

--- modules/aca/exchange_booking.rb
+++ modules/aca/exchange_booking.rb
@@ -96,6 +96,7 @@ class Aca::ExchangeBooking
         self[:hide_all] = setting(:hide_all) || false
         self[:touch_enabled] = setting(:touch_enabled) || false
         self[:name] = self[:room_name] = setting(:room_name) || system.name
+        self[:description] = setting(:description) || nil
         self[:timeout] = setting(:timeout) || false
 
         self[:control_url] = setting(:booking_control_url) || system.config.support_url
