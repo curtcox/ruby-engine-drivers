41b4ea24960d20f2dd218e673856e263ebe1958f
Stephen von Takach <steve@cotag.me>
2018-05-02 14:13:29 +1000

    (epiphan:pearl) keep track of recording status
    on multiple channels

--- modules/epiphan/pearl2.rb
+++ modules/epiphan/pearl2.rb
@@ -34,7 +34,7 @@ class Epiphan::Pearl2
     def status(channel = 1)
         exec('recorder_status', channel) do |data|
             detail = JSON.parse(data.body, symbolize_names: true)
-            self[:recording] = !detail[:state].empty?
+            self["channel#{channel}"] = detail[:state].present? ? :recording : :idle
             self[:time] = detail[:time]
             self[:total] = detail[:total]
             self[:state] = detail[:state]
@@ -44,7 +44,6 @@ class Epiphan::Pearl2
 
     def record(channel = 1)
         exec('start_recorder', channel) do |data|
-            self[:recording] = true
             self["channel#{channel}"] = :recording
             status
         end
