3880a4e6ab89d496b8063931f205426564feeb80
Stephen von Takach <steve@acaprojects.com>
2018-05-31 07:47:35 +1000

    (cisco:vidconf) ensure communication settings
    are correct for the SSH version of the module

--- modules/cisco/tele_presence/sx_series_ssh.rb
+++ modules/cisco/tele_presence/sx_series_ssh.rb
@@ -5,6 +5,9 @@ load File.expand_path('./sx_series_common.rb', File.dirname(__FILE__))
 class Cisco::TelePresence::SxSeriesSsh < Cisco::TelePresence::SxSsh
     include Cisco::TelePresence::SxSeriesCommon
 
+    # Communication settings
+    tokenize delimiter: "\r"
+
     # Discovery Information
     descriptive_name 'Cisco TelePresence (secure)'
     generic_name :VidConf
