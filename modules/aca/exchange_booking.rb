b405c130e5f05bf9cface854b237fb1a4c7228c6
Cameron Reeves <c@meronreev.es>
2018-06-07 16:10:41 +1000

    Fix syntax for all day check

--- modules/aca/exchange_booking.rb
+++ modules/aca/exchange_booking.rb
@@ -719,7 +719,7 @@ class Aca::ExchangeBooking
         results = items.collect do |meeting|
             all_day_bookings = ENV['ALL_DAY_BOOKINGS'] || true
             if !all_day_bookings
-                next if meeting.ews_item.all_day?
+                next if meeting.all_day?
             end
             item = meeting.ews_item
             start = item[:start][:text]
