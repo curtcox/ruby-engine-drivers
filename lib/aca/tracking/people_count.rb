a4a8089bcc5867342c3eb01a4c1727b1ab9819f6
Cameron Reeves <c@meronreev.es>
2018-06-13 19:19:39 +1000

    Add people count module (oops it was in the last commit)

---
+++ lib/aca/tracking/people_count.rb
@@ -0,0 +1,14 @@
+class Aca::Tracking::PeopleCount < CouchbaseOrm::Base
+    design_document :pcount
+
+    # Connection details
+    attribute :room_email,   type: String  
+    attribute :booking_id,   type: String  
+    attribute :system_id,    type: String  
+    attribute :capacity,     type: Integer 
+    attribute :maximum,      type: Integer 
+    attribute :average,      type: Integer 
+    attribute :median,       type: Integer
+    attribute :organiser,    type: String
+    attribute :counts,       type: Array, default: []
+end
