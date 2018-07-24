d1a3b1adf8f5bfac663bf56d6f085dd9d632fbdc
Stephen von Takach <steve@cotag.me>
2018-07-24 16:43:42 +1000

    (cisco:snmp snooping) tweak default timeout

--- modules/cisco/switch/snooping_catalyst_snmp.rb
+++ modules/cisco/switch/snooping_catalyst_snmp.rb
@@ -29,7 +29,7 @@ class Cisco::Switch::SnoopingCatalystSNMP
             community: 'public'
         },
         # Snooping takes ages on large switches
-        response_timeout: 3000
+        response_timeout: 5000
     })
 
     def on_load
@@ -358,7 +358,7 @@ class Cisco::Switch::SnoopingCatalystSNMP
 
         settings = setting(:snmp_options).to_h.symbolize_keys
         @transport&.close
-        @transport = settings[:proxy] = Protocols::Snmp.new(self, setting(:response_timeout) || 3000)
+        @transport = settings[:proxy] = Protocols::Snmp.new(self, setting(:response_timeout) || 5000)
         @transport.register(@resolved_ip, remote_port)
         @client = NETSNMP::Client.new(settings)
         @community = settings[:community]
