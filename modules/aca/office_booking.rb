2526fe900dc693e23cb268da5bbe5929e5a3ecc5
Cameron Reeves <c@meronreev.es>
2018-07-30 15:32:32 +1000

    [O365 Driver] Add logging helper

--- modules/aca/office_booking.rb
+++ modules/aca/office_booking.rb
@@ -618,10 +618,13 @@ class Aca::OfficeBooking
         # end
         delete_at_object = Time.at(delete_at).utc.iso8601
         if self[:today]
-            self[:today].each do |booking|
+            self[:today].each_with_index do |booking, i|
                 if delete_at_object.to_i == booking[:Start].to_i
                     response = @client.delete_booking(booking_id: booking[:id], current_user: system)
-                    count += 1 if response == 200
+                    if response == 200
+                        count += 1
+                        self[:today].delete(i)
+                    end
                 end
             end
         end
