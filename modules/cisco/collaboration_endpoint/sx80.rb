c7e81e4eeede315931eb877bff88a29d54fc6960
Kim Burgess <kim@acaprojects.com>
2018-09-24 18:27:20 +1000

    (cisco:ce) switch speaker track control to on/off rather than enable/disable

--- modules/cisco/collaboration_endpoint/sx80.rb
+++ modules/cisco/collaboration_endpoint/sx80.rb
@@ -133,11 +133,14 @@ class Cisco::CollaborationEndpoint::Sx80 < Cisco::CollaborationEndpoint::RoomOs
     command! 'Cameras SpeakerTrack Diagnostics Stop' => \
              :speaker_track_diagnostics_stop
 
-    # The 'integrator' account can't active/deactive SpeakerTrack, but we can
-    # cut off access via a configuration setting.
+    command 'Cameras SpeakerTrack Activate' => :speaker_track_activate
+    command 'Cameras SpeakerTrack Deactivate' => :speaker_track_deactivate
     def speaker_track(state = On)
-        mode = is_affirmative?(state) ? :Auto : :Off
-        send_xconfiguration 'Cameras SpeakerTrack', :Mode, mode
+        if is_affirmative? state
+            speaker_track_activate
+        else
+            speaker_track_deactivate
+        end
     end
 
     command 'Conference DoNotDisturb Activate' => :do_not_disturb_activate,
