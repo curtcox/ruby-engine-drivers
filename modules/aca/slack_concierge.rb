f509632b938b20b74e58314f6e395fd1784bcfce
Cameron Reeves <c@meronreev.es>
2018-12-04 15:11:48 +1100

    [Slack Modules] Fix syntax and logic

--- modules/aca/slack_concierge.rb
+++ modules/aca/slack_concierge.rb
@@ -170,7 +170,7 @@ class Aca::SlackConcierge
                             end
                         end
                     else
-                        data['replies'] ||= []
+                        data['replies'] ||= get_message(data['ts'])
                         self["threads"].insert(0,data)
                     end    
                 end                
