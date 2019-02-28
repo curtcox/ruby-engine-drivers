f834bf2a71574d311e56ac0df902f48c691cd74d
Kim Burgess <kim@acaprojects.com>
2019-02-28 22:28:34 +1000

    (cisco:sx80,room_kit) add video layout control

--- modules/cisco/collaboration_endpoint/room_kit.rb
+++ modules/cisco/collaboration_endpoint/room_kit.rb
@@ -55,6 +55,7 @@ class Cisco::CollaborationEndpoint::RoomKit < Cisco::CollaborationEndpoint::Room
     status 'Video Selfview FullScreenMode' => :selfview_fullscreen
     status 'Video Input' => :video_input
     status 'Video Output' => :video_output
+    status 'Video Layout LayoutFamily Local' => :video_layout
     status 'Standby State' => :standby
 
     command 'Audio Microphones Mute' => :mic_mute_on
@@ -122,6 +123,10 @@ class Cisco::CollaborationEndpoint::RoomKit < Cisco::CollaborationEndpoint::Room
             ConnectorId_: (1..3),       # Source can either be specified as the
             Layout_: [:Equal, :PIP],    # physical connector...
             SourceId_: (1..3)           # ...or the logical source ID
+
+    command 'Video Layout LayoutFamily Set' => :video_layout,
+            LayoutFamily: [:Auto, :Equal, :Overlay, :Prominent, :Single],
+            Target_: [:Local, :Remote]
 
     command 'Video Selfview Set' => :selfview,
             Mode_: [:On, :Off],
--- modules/cisco/collaboration_endpoint/sx80.rb
+++ modules/cisco/collaboration_endpoint/sx80.rb
@@ -53,6 +53,7 @@ class Cisco::CollaborationEndpoint::Sx80 < Cisco::CollaborationEndpoint::RoomOs
     status 'Video Selfview FullScreenMode' => :selfview_fullscreen
     status 'Video Input' => :video_input
     status 'Video Output' => :video_output
+    status 'Video Layout LayoutFamily Local' => :video_layout
     status 'Standby State' => :standby
 
     command 'Audio Microphones Mute' => :mic_mute_on
@@ -120,6 +121,10 @@ class Cisco::CollaborationEndpoint::Sx80 < Cisco::CollaborationEndpoint::RoomOs
             ConnectorId_: (1..5),       # Source can either be specified as the
             Layout_: [:Equal, :PIP],    # physical connector...
             SourceId_: (1..4)           # ...or the logical source ID
+
+    command 'Video Layout LayoutFamily Set' => :video_layout,
+            LayoutFamily: [:Auto, :Equal, :Overlay, :Prominent, :Single],
+            Target_: [:Local, :Remote]
 
     command 'Video Selfview Set' => :selfview,
             Mode_: [:On, :Off],
