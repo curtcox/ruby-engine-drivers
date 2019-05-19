6910788c4313dd3e31ae330fe61cc36bf88e58fe
Kim Burgess <kim@acaprojects.com>
2019-05-19 21:01:59 +1000

    (extron:sw) add driver for Extron SW series switchers

---
+++ modules/extron/switcher/sw.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+load File.expand_path('../base.rb', File.dirname(__FILE__))
+
+module Extron::Switcher; end
+
+class Extron::Switcher::Sw < Extron::Base
+    descriptive_name 'Extron Switcher SW'
+    generic_name :Switcher
+
+    def switch_to(input)
+        do_send "#{input}!"
+    end
+
+    def mute_video(state = true)
+        val = is_affirmative?(state) ? 1 : 0
+        do_send "#{val}B"
+    end
+
+    def unmute_video
+        mute_video false
+    end
+
+    def mute_audio(state = true)
+        val = is_affirmative?(state) ? 1 : 0
+        do_send "#{val}Z"
+    end
+
+    def unmute_audio
+        mute_audio false
+    end
+
+    def received(data, resolve, command)
+        logger.debug { "Extron switcher sent #{data}" }
+
+        if data =~ /Login/i
+            device_ready
+        else
+            response, _, param = data.partition(/(?=\d)/)
+
+            case response.to_sym
+            when :In
+                self[:input] = param.to_i
+            when :Vmt
+                self[:video_muted] = param > '0'
+            when :Amt
+                self[:audio_muted] = param > '0'
+            when :Sig
+                param.split.each_with_index do |state, idx|
+                    self[:"input_#{idx + 1}_sync"] = state == '1'
+                end
+            when :Hdcp
+                param.split.each_with_index do |state, idx|
+                    self[:"input_#{idx + 1}_hdcp"] = state == '1'
+                end
+            when :E
+                code = param.to_i
+                logger.warn(ERROR[code] || "Unknown device error (#{code})")
+                return :failed
+            else
+                logger.info("Unhandled device response (#{data})")
+            end
+        end
+
+        :success
+    end
+
+
+    ERRORS = {
+        1 => 'Invalid input channel (out of range)',
+        6 => 'Invalid input during auto-input switching',
+        10 => 'Invalid command',
+        13 => 'Invalid value (out of range)'
+    }.freeze
+end
