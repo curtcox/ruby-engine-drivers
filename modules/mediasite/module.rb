2e04c796400445c92dde0ce4793f7d53e1567c4e
Philip Kheav <philip.kheav@hotmail.com>
2019-05-29 15:54:07 +1000

    add function to determine if presentation is live

--- modules/mediasite/module.rb
+++ modules/mediasite/module.rb
@@ -43,7 +43,7 @@ class Mediasite::Module
     end
 
     def get_request(url)
-        req_url = setting(:url) + url
+        req_url = url
         logger.debug(req_url)
         uri = URI(req_url)
         req = Net::HTTP::Get.new(uri)
@@ -54,20 +54,18 @@ class Mediasite::Module
         JSON.parse(http.request(req).body)
     end
 
+    def create_url(url)
+        setting(:url) + url
+    end
+
     def get_device_id
         # Use $top=1000 to ensure that all rooms are returned from the api
-        res = get_request('api/v1/Rooms?$top=1000')
+        res = get_request(create_url('api/v1/Rooms?$top=1000'))
         res['value'].each { |room|
             if room['Name'] == room_name
                 return room['DeviceConfigurations'][0]['DeviceId']
             end
         }
-    end
-
-    def get_device
-        res = get_request("api/v1/Recorders('#{self[:device_id]}')")
-        logger.debug res
-        res
     end
 
     def post_request(url)
@@ -98,18 +96,40 @@ class Mediasite::Module
     }.freeze
 
     def state
-        res = get_request("/api/v1/Recorders('#{self[:device_id]}')/Status")
+        res = get_request(create_url("/api/v1/Recorders('#{self[:device_id]}')/Status"))
         self[:previous_state] = self[:state]
         self[:state] = STATES[res['RecorderState']]
-        res = get_request("/api/v1/Recorders('#{self[:device_id]}')/CurrentPresentationMetadata")
+
+        res = get_request(create_url("/api/v1/Recorders('#{self[:device_id]}')/CurrentPresentationMetadata"))
         self[:title] = res['Title']
         self[:presenters] = res['Presenters']
-        self[:live] = false # TODO: found out how to know if recording is being live streamed
-        res = get_request("/api/v1/Recorders('#{self[:device_id]}')/ActiveInputs")
+
+        # TODO: found out how to know if recording is being live streamed
+        self[:live] = live?
+
+        res = get_request(create_url("/api/v1/Recorders('#{self[:device_id]}')/ActiveInputs"))
         self[:dual] = res['ActiveInputs'].size >= 2
-        res = get_request("/api/v1/Recorders('#{self[:device_id]}')/TimeRemaining")
+
+        res = get_request(create_url("/api/v1/Recorders('#{self[:device_id]}')/TimeRemaining"))
         self[:time_remaining] = res['SecondsRemaining']
-        self[:volume] = 0
+
+        self[:volume] = 0 # TODO:
+    end
+
+    def live?
+        live = false
+        res = get_request(create_url("/api/v1/Recorders('#{self[:device_id]}')/ScheduledRecordingTimes"))
+        res['value'].each { |schedule|
+            current_time = ActiveSupport::TimeZone.new('UTC').now
+            start_time = ActiveSupport::TimeZone.new('UTC').parse(schedule['StartTime'])
+            end_time = ActiveSupport::TimeZone.new('UTC').parse(schedule['EndTime'])
+            if start_time <= current_time && current_time <= end_time
+                presentation = get_request(schedule['ScheduleLink'] + "/Presentations")
+                live = presentation['value']['Status'] == 'Live'
+                break;
+            end
+        }
+        live
     end
 
     def start
