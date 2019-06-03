3b8e82ff4ddae0b13bb0872d0b4faa6ca2f7dfc4
Stephen von Takach <steve@acaprojects.com>
2019-06-03 17:31:46 +1000

    (messagemedia:sms) revert name

--- modules/message_media/sms.rb
+++ modules/message_media/sms.rb
@@ -8,8 +8,8 @@ class MessageMedia::SMS
 
     # Discovery Information
     implements :service
-    descriptive_name 'Pexip Management API'
-    generic_name :Pexip
+    descriptive_name 'MessageMedia SMS service'
+    generic_name :SMS
 
     # HTTP keepalive
     keepalive false
