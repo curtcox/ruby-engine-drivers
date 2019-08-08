d1185187cc6f78806bf27519389d6bc0581a62b6
William Le <w.le@acaprojects.com>
2019-08-08 18:12:02 +0800

    EWS>RBP>cleanup: slightly clearer func/var names

--- modules/aca/exchange_booking.rb
+++ modules/aca/exchange_booking.rb
@@ -36,7 +36,10 @@ class Aca::ExchangeBooking
     end
 
     def on_update
-        @use_act_as = setting(:use_act_as)
+        # Set to true if the EWS service account does not have access to directly read room mailboxes, but has access to impersonate room mailboxes
+        # https://docs.microsoft.com/en-us/exchange/client-developer/exchange-web-services/impersonation-and-ews-in-exchange
+        @ews_impersonate_room = setting(:ews_impersonate_room) || setting(:use_act_as)  
+        
         self[:room_name] = setting(:room_name) || system.name
         self[:hide_all] = setting(:hide_all) || false
         self[:touch_enabled] = setting(:touch_enabled) || false
@@ -75,6 +78,10 @@ class Aca::ExchangeBooking
         self[:booking_default_title] = setting(:booking_default_title) || "On the spot booking"
         self[:booking_select_free] = setting(:booking_select_free)
         self[:booking_hide_all] = setting(:booking_hide_all) || false
+
+        self[:last_meeting_started] = setting(:last_meeting_started)
+        self[:cancel_meeting_after] = setting(:cancel_meeting_after)
+
         @hide_all_day_bookings = Boolean(setting(:hide_all_day_bookings))
 
         @check_meeting_ending = setting(:check_meeting_ending) # seconds before meeting ending
@@ -87,46 +94,29 @@ class Aca::ExchangeBooking
                 setting(:ews_password),
                 { http_opts: { ssl_verify_mode: 0 } }
             ]
-            @ews_room = setting(:room_mailbox) || system.email
+            @room_mailbox = setting(:room_mailbox) || system.email
             @ews_connect_type = (setting(:ews_connect_type) || :SMTP).to_sym    # supports: SMTP, PSMTP, SID, UPN
             @timezone = setting(:timezone) || ENV['TZ']
         else
             logger.error "Viewpoint gem not available"
         end
 
-        self[:last_meeting_started] = setting(:last_meeting_started)
-        self[:cancel_meeting_after] = setting(:cancel_meeting_after)
-
         schedule.clear
-        schedule.in(rand(10000)) {
-            fetch_bookings(true)
-        }
-        schedule.every((setting(:update_every) || 120000).to_i + rand(10000)) {
-            fetch_bookings
-        }
+        schedule.in(rand(10000)) { fetch_bookings }
+        fetch_interval = UV::Scheduler.parse_duration(setting(:update_every)) + rand(10000)
+        schedule.every(fetch_interval) { fetch_bookings }
     end
 
-
-
-    # ======================================
-    # ROOM BOOKINGS:
-    # ======================================
-    def fetch_bookings(first=false)
-        logger.debug { "looking up todays emails for #{@ews_room}" }
+    def fetch_bookings
+        raise "RBP>#{system.id} (#{system.name})>: Room mailbox not configured" unless @room_mailbox
+        logger.debug { "looking up todays emails for #{@room_mailbox}" }
         task {
-            todays_bookings(first)
+            todays_bookings
         }.then(proc { |bookings|
             self[:today] = bookings
-            if @check_meeting_ending
-                should_notify?
-            end
+            meeting_extendable? if @check_meeting_ending
         }, proc { |e| logger.print_error(e, 'error fetching bookings') })
     end
-
-
-    # ======================================
-    # Meeting Helper Functions
-    # ======================================
 
     def start_meeting(meeting_ref)
         self[:last_meeting_started] = meeting_ref
@@ -142,15 +132,12 @@ class Aca::ExchangeBooking
                 start_time = (start_time / 1000).to_i
                 delete_ews_booking start_time
             else
-                # Converts to time object regardless of start_time being string or time object
                 start_time = Time.parse(start_time.to_s).to_i
                 delete_ews_booking start_time
             end
         }.then(proc { |count|
             logger.warn { "successfully removed #{count} bookings due to #{reason}" }
-
             start_meeting(start_time * 1000)
-
             fetch_bookings
             true
         }, proc { |error|
@@ -183,31 +170,209 @@ class Aca::ExchangeBooking
     def clear_end_meeting_warning
         self[:meeting_ending] = self[:last_meeting_started]
     end
-    # ---------
 
     def create_meeting(options)
+        raise "RBP>#{system.id} (#{system.name})>: Room mailbox not configured" unless @room_mailbox
         # Check that the required params exist
         required_fields = [:start, :end]
         check = required_fields - options.keys.collect(&:to_sym)
         if check != []
             # There are missing required fields
-            logger.info "Required fields missing: #{check}"
+            logger.error "Required fields missing: #{check}"
             raise "missing required fields: #{check}"
         end
 
         req_params = {}
-        req_params[:room_email] = @ews_room
+        req_params[:room_email] = @room_mailbox
         req_params[:subject] = options[:title]
         req_params[:start_time] = Time.at(options[:start].to_i / 1000).utc.iso8601.chop
         req_params[:end_time] = Time.at(options[:end].to_i / 1000).utc.iso8601.chop
 
         make_ews_booking req_params
-        fetch_bookings
-        logger.debug { "successfully created booking: #{id}" }
+        logger.info { "successfully created booking: #{id}" }
+        schedule.in('2s') do
+            fetch_bookings
+        end
         "Ok"
     end
 
-    def should_notify?
+    def extend_meeting
+        starting = self[:meeting_canbe_extended]
+        return false unless starting
+
+        ending = starting + @extend_meeting_by
+        create_meeting(start: starting * 1000, end: ending * 1000, title: @current_meeting_title).then do
+            start_meeting(starting * 1000)
+        end
+    end
+
+    def send_email(title, body, to)
+        raise "RBP>#{system.id} (#{system.name})>: Room mailbox not configured" unless @room_mailbox
+        task {
+            cli = Viewpoint::EWSClient.new(*@ews_creds)
+            opts = {}
+
+            if @ews_impersonate_room
+                opts[:act_as] = @room_mailbox
+            else
+                cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @room_mailbox)
+            end
+
+            cli.send_message subject: title, body: body, to_recipients: to
+        }
+    end
+
+
+    protected
+
+
+    # =======================================
+    # EWS Requests to occur in a worker thread
+    # =======================================
+    def make_ews_booking(user_email: nil, subject: self[:booking_default_title], room_email:, start_time:, end_time:)
+        user_email ||= self[:email]  # if swipe card used
+
+        booking = {
+            subject: subject,
+            start: start_time,
+            end: end_time
+        }
+
+        if user_email
+            booking[:required_attendees] = [{
+                attendee: { mailbox: { email_address: user_email } }
+            }]
+        end
+
+        cli = Viewpoint::EWSClient.new(*@ews_creds)
+        opts = {}
+
+        if @ews_impersonate_room
+            opts[:act_as] = @room_mailbox
+        else
+            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @room_mailbox)
+        end
+
+        folder = cli.get_folder(:calendar, opts)
+        appointment = folder.create_item(booking)
+
+        # Return the booking IDs
+        appointment.item_id
+    end
+
+    def delete_ews_booking(delete_at)
+        now = Time.now
+        timeout = delete_at + (self[:booking_cancel_timeout] || self[:timeout]) + 120
+        if now.to_i > timeout
+          start_meeting(delete_at * 1000)
+          return 0
+        end
+
+        if @timezone
+            start  = now.in_time_zone(@timezone).midnight
+            ending = now.in_time_zone(@timezone).tomorrow.midnight
+        else
+            start  = now.midnight
+            ending = now.tomorrow.midnight
+        end
+
+        count = 0
+
+        cli = Viewpoint::EWSClient.new(*@ews_creds)
+
+        if @ews_impersonate_room
+            opts = {}
+            opts[:act_as] = @room_mailbox
+
+            folder = cli.get_folder(:calendar, opts)
+            items = folder.items({:calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
+        else
+            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @room_mailbox)
+            items = cli.find_items({:folder_id => :calendar, :calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
+        end
+
+        items.each do |meeting|
+            meeting_time = Time.parse(meeting.ews_item[:start][:text])
+
+            # Remove any meetings that match the start time provided
+            if meeting_time.to_i == delete_at
+                # new_booking = meeting.update_item!({ end: Time.now.utc.iso8601.chop })
+
+                meeting.delete!(:recycle, send_meeting_cancellations: 'SendOnlyToAll')
+                count += 1
+            end
+        end
+        count
+    end
+
+    def todays_bookings
+        now = Time.now
+        if @timezone
+            start  = now.in_time_zone(@timezone).midnight
+            ending = now.in_time_zone(@timezone).tomorrow.midnight
+        else
+            start  = now.midnight
+            ending = now.tomorrow.midnight
+        end
+
+        cli = Viewpoint::EWSClient.new(*@ews_creds)
+
+
+        if @ews_impersonate_room
+            opts = {}
+            opts[:act_as] = @room_mailbox
+
+            folder = cli.get_folder(:calendar, opts)
+            items = folder.items({:calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
+        else
+            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @room_mailbox)
+            items = cli.find_items({:folder_id => :calendar, :calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
+        end
+
+        now_int = now.to_i
+
+        results = items.collect do |meeting|
+            item = meeting.ews_item
+            start = item[:start][:text]
+            ending = item[:end][:text]
+
+            real_start = Time.parse(start)
+            real_end = Time.parse(ending)
+
+            if @timezone
+                start = Time.parse(start).in_time_zone(@timezone).iso8601[0..18]
+                ending = Time.parse(ending).in_time_zone(@timezone).iso8601[0..18]
+            end
+
+            logger.debug { item.inspect }
+
+            if @hide_all_day_bookings
+                next if Time.parse(ending) - Time.parse(start) > 86399
+            end
+
+            if ["Private", "Confidential"].include?(meeting.sensitivity)
+                subject = meeting.sensitivity
+                booking_owner = "Private"
+            else
+                subject = item[:subject][:text]
+                booking_owner = item[:organizer][:elems][0][:mailbox][:elems][0][:name][:text]
+            end
+
+            {
+                :Start => start,
+                :End => ending,
+                :Subject => subject,
+                :owner => booking_owner,
+                :setup => 0,
+                :breakdown => 0,
+                :start_epoch => real_start.to_i,
+                :end_epoch => real_end.to_i
+            }
+        end
+        results.compact!
+    end
+
+    def meeting_extendable?
         bookings = self[:today]
         return unless bookings.present?
         now = Time.now.to_i
@@ -244,185 +409,5 @@ class Aca::ExchangeBooking
         else
             self[:meeting_canbe_extended] = false
         end
-    end
-
-    def extend_meeting
-        starting = self[:meeting_canbe_extended]
-        return false unless starting
-
-        ending = starting + @extend_meeting_by
-        create_meeting(start: starting * 1000, end: ending * 1000, title: @current_meeting_title).then do
-            start_meeting(starting * 1000)
-        end
-    end
-
-    def send_email(title, body, to)
-        task {
-            cli = Viewpoint::EWSClient.new(*@ews_creds)
-            opts = {}
-
-            if @use_act_as
-                opts[:act_as] = @ews_room if @ews_room
-            else
-                cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @ews_room) if @ews_room
-            end
-
-            cli.send_message subject: title, body: body, to_recipients: to
-        }
-    end
-
-
-    protected
-
-
-    # =======================================
-    # EWS Requests to occur in a worker thread
-    # =======================================
-    def make_ews_booking(user_email: nil, subject: 'On the spot booking', room_email:, start_time:, end_time:)
-        user_email ||= self[:email]  # if swipe card used
-
-        booking = {
-            subject: subject,
-            start: start_time,
-            end: end_time
-        }
-
-        if user_email
-            booking[:required_attendees] = [{
-                attendee: { mailbox: { email_address: user_email } }
-            }]
-        end
-
-        cli = Viewpoint::EWSClient.new(*@ews_creds)
-        opts = {}
-
-        if @use_act_as
-            opts[:act_as] = @ews_room if @ews_room
-        else
-            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @ews_room) if @ews_room
-        end
-
-        folder = cli.get_folder(:calendar, opts)
-        appointment = folder.create_item(booking)
-
-        # Return the booking IDs
-        appointment.item_id
-    end
-
-    def delete_ews_booking(delete_at)
-        now = Time.now
-        timeout = delete_at + (self[:booking_cancel_timeout] || self[:timeout]) + 120
-        if now.to_i > timeout
-          start_meeting(delete_at * 1000)
-          return 0
-        end
-
-        if @timezone
-            start  = now.in_time_zone(@timezone).midnight
-            ending = now.in_time_zone(@timezone).tomorrow.midnight
-        else
-            start  = now.midnight
-            ending = now.tomorrow.midnight
-        end
-
-        count = 0
-
-        cli = Viewpoint::EWSClient.new(*@ews_creds)
-
-        if @use_act_as
-            # TODO:: think this line can be removed??
-            # delete_at = Time.parse(delete_at.to_s).to_i
-
-            opts = {}
-            opts[:act_as] = @ews_room if @ews_room
-
-            folder = cli.get_folder(:calendar, opts)
-            items = folder.items({:calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
-        else
-            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @ews_room) if @ews_room
-            items = cli.find_items({:folder_id => :calendar, :calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
-        end
-
-        items.each do |meeting|
-            meeting_time = Time.parse(meeting.ews_item[:start][:text])
-
-            # Remove any meetings that match the start time provided
-            if meeting_time.to_i == delete_at
-                # new_booking = meeting.update_item!({ end: Time.now.utc.iso8601.chop })
-
-                meeting.delete!(:recycle, send_meeting_cancellations: 'SendOnlyToAll')
-                count += 1
-            end
-        end
-        count
-    end
-
-    def todays_bookings(first=false)
-        now = Time.now
-        if @timezone
-            start  = now.in_time_zone(@timezone).midnight
-            ending = now.in_time_zone(@timezone).tomorrow.midnight
-        else
-            start  = now.midnight
-            ending = now.tomorrow.midnight
-        end
-
-        cli = Viewpoint::EWSClient.new(*@ews_creds)
-
-
-        if @use_act_as
-            opts = {}
-            opts[:act_as] = @ews_room if @ews_room
-
-            folder = cli.get_folder(:calendar, opts)
-            items = folder.items({:calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
-        else
-            cli.set_impersonation(Viewpoint::EWS::ConnectingSID[@ews_connect_type], @ews_room) if @ews_room
-            items = cli.find_items({:folder_id => :calendar, :calendar_view => {:start_date => start.utc.iso8601, :end_date => ending.utc.iso8601}})
-        end
-
-        now_int = now.to_i
-
-        # items.select! { |booking| !booking.cancelled? }
-        results = items.collect do |meeting|
-            item = meeting.ews_item
-            start = item[:start][:text]
-            ending = item[:end][:text]
-
-            real_start = Time.parse(start)
-            real_end = Time.parse(ending)
-
-            # Extract the skype meeting URL
-            if @timezone
-                start = Time.parse(start).in_time_zone(@timezone).iso8601[0..18]
-                ending = Time.parse(ending).in_time_zone(@timezone).iso8601[0..18]
-            end
-
-            logger.debug { item.inspect }
-
-            if @hide_all_day_bookings
-                next if Time.parse(ending) - Time.parse(start) > 86399
-            end
-
-            if ["Private", "Confidential"].include?(meeting.sensitivity)
-                subject = meeting.sensitivity
-                booking_owner = "Private"
-            else
-                subject = item[:subject][:text]
-                booking_owner = item[:organizer][:elems][0][:mailbox][:elems][0][:name][:text]
-            end
-
-            {
-                :Start => start,
-                :End => ending,
-                :Subject => subject,
-                :owner => booking_owner,
-                :setup => 0,
-                :breakdown => 0,
-                :start_epoch => real_start.to_i,
-                :end_epoch => real_end.to_i
-            }
-        end
-        results.compact!
     end
 end
